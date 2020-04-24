d60b6ccc72fc1b09ba344514ba322c90d48819cf
randynetworks <randynetworks@gmail.com>
2020-04-25 06:10:38 +0700

    day 2 | calculator sederhana

---
+++ day2/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+// prototype
+void startCalc();
+void penjumlahan(float a, float b);
+void pengurangan(float a, float b);
+void perkalian(float a, float b);
+void pembagian(float a, float b);
+
+// membuat kalkulator sederhana
+int main(int argc, char const *argv[])
+{
+    startCalc();
+
+    return 0;
+}
+
+void startCalc(){
+    int opt;
+    float a,b;
+    cout << "=Selamat datang di calculator sederhana=" << endl;
+    cout << "Masukan angka 1: ";
+    cin >> a;
+    cout << "Masukan angka 2: ";
+    cin >> b;
+    cout << "----------1. + 2. - 3. * 4. /-----------" << endl;
+    cout << "Pilih perhitungan: ";
+    cin >> opt;
+
+    if (opt == 1)
+    {
+        penjumlahan(a,b);
+        startCalc();
+    } else if (opt == 2)
+    {
+        pengurangan(a,b);
+        startCalc();
+    } else if (opt == 3)
+    {
+        perkalian(a,b);
+        startCalc();
+    } else if (opt == 4)
+    {
+        pembagian(a,b);
+        startCalc();
+    } else {
+        cout << "Yang kamu masukan salah broh!" << endl;
+        startCalc();
+    }
+    
+}
+void penjumlahan(float a, float b){
+    cout << "Hasil penjumlahannya: " << a + b << endl;
+}
+
+void pengurangan(float a, float b){
+    cout << "Hasil pengurangannya: " << a - b << endl;
+}
+void perkalian(float a, float b){
+    cout << "Hasil perkaliannya: " << a * b << endl;
+}
+void pembagian(float a, float b){
+    cout << "Hasil pembagiannya: " << a / b << endl;
+}
